2bf590a19f135f5251912c6afc55a6095773f88a
Privera1999 <pabloriveraj1999@gmail.com>
2023-11-07 20:23:20 +0100

    Aplicacion

--- app/src/main/java/com/pmydm/practica04/MainActivity.kt
+++ app/src/main/java/com/pmydm/practica04/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -26,6 +33,7 @@ import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.InspectableModifier
@@ -101,23 +109,62 @@ fun MuyBuscado(
         modifier = modifier
     ){
         Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.width(255.dp)
+            modifier = Modifier
+                .width(140.dp)
+                .height(100.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(drawable),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(80.dp)
-
-            )
             Text(text= stringResource(text),
                 style = MaterialTheme.typography.titleMedium,
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Image(
+                modifier = Modifier,
+                painter = painterResource(drawable),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight
+
+
+
+            )
+
         }
     }
 }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
+private val muybuscadoColeccion = listOf(
+    R.drawable.muybuscado to R.string.Iphone,
+    R.drawable.coche to R.string.Coche,
+    R.drawable.switch1 to R.string.Switch,
+
+).map { DrawableStringPair(it.first, it.second) }
+
+@Composable
+fun MuyBuscadoGrid(
+    modifier: Modifier = Modifier
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(1),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(100.dp)
+    ){
+        items(muybuscadoColeccion){
+                item ->
+            MuyBuscado(item.drawable,item.text)
+        }
+    }
+}
+
 
 
 @Preview(showBackground = true)
@@ -132,7 +179,15 @@ fun GreetingPreview() {
 @Composable
 fun MuyBuscado1() {
     Practica04Theme {
-        MuyBuscado(drawable = R.drawable.pruebamuybuscado, text = R.string.Iphone,
+        MuyBuscado(drawable = R.drawable.muybuscado, text = R.string.Iphone,
             modifier = Modifier.padding(8.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun muybuscadosGrid() {
+    Practica04Theme {
+        MuyBuscadoGrid()
     }
 }
