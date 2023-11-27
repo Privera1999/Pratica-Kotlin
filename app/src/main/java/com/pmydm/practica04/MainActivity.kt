9cfa19a581d8500974489ec61fe817f5bfe01cea
Privera1999 <pabloriveraj1999@gmail.com>
2023-11-27 20:15:14 +0100

    .

--- app/src/main/java/com/pmydm/practica04/MainActivity.kt
+++ app/src/main/java/com/pmydm/practica04/MainActivity.kt
@@ -5,9 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -16,10 +16,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -28,8 +26,6 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -40,7 +36,6 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -51,23 +46,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.InspectableModifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -172,6 +161,7 @@ fun Categorias(
                 .width(35.dp)
                 .clip(CircleShape)
                 .border(1.dp, Color.Black, shape = CircleShape)
+                .clickable { categoria++ }
 
         )
         Text(
@@ -236,6 +226,15 @@ private val DestacadosColecction = listOf(
 
     )
 
+private val DestacadosColecctionMotos = listOf(
+    Destacadosclass(R.drawable.coche1, R.string.motoprecio, R.string.moto),
+    Destacadosclass(R.drawable.coche2, R.string.KiaPrecio, R.string.KiaNombre),
+    Destacadosclass(R.drawable.coche3, R.string.PorschePrecio, R.string.PorscheNombre),
+    Destacadosclass(R.drawable.coche4, R.string.lamborghiniPrecio, R.string.lamborghiniNombre),
+    Destacadosclass(R.drawable.coche5, R.string.BmwPrecio, R.string.BmwNombre),
+
+    )
+
 @Composable
 fun MuyBuscadoGrid(
     modifier: Modifier = Modifier
@@ -263,6 +262,8 @@ fun MuyBuscadoGrid(
         }
     }
 }
+
+
 
 @Composable
 fun Destacados(
@@ -302,8 +303,6 @@ fun Destacados(
                         .paddingFromBaseline(top = 0.dp, bottom = 8.dp)
                         .align(Alignment.Start),
                     style = MaterialTheme.typography.bodyMedium)
-
-
         Text(
             text= "Destacado",
             modifier = Modifier
@@ -311,19 +310,18 @@ fun Destacados(
                 .align(Alignment.Start),
             style = MaterialTheme.typography.bodySmall)
 
-
         var favorito by rememberSaveable { mutableStateOf(0) }
 
         if(favorito==0){
             IconButton(onClick = {favorito++},
                 modifier.align(Alignment.End)) {
-                Icon(Icons.Default.FavoriteBorder,contentDescription = "")
+                Icon(Icons.Default.FavoriteBorder,contentDescription = "",tint = Color.Red)
             }
 
         }else{
             IconButton(onClick = {favorito--},
                 modifier.align(Alignment.End)) {
-                Icon(Icons.Default.Favorite,contentDescription = "")
+                Icon(Icons.Default.Favorite,contentDescription = "",tint = Color.Red)
             }
         }
 
@@ -371,10 +369,10 @@ fun productoscerca(
 }
 
 
-
 @Composable
 fun DestacadoGrid(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+
 ) {
     Text(
         text = "Destacados",
@@ -384,15 +382,25 @@ fun DestacadoGrid(
         style = MaterialTheme.typography.titleLarge
     )
 
-    LazyRow(modifier = modifier,
+    var categoria by remember { mutableStateOf(0) }
+
+
+    LazyRow(
+        modifier = modifier,
         contentPadding = PaddingValues(horizontal = 16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ){
-        items(DestacadosColecction){
-                item -> Destacados(item.imagenResId,item.nombreResId,item.precioResId)
+    ) {
+        if (categoria == 0) {
+            items(DestacadosColecction) { item ->
+                Destacados(item.imagenResId, item.nombreResId, item.precioResId)
+            }
+        } else {
+            items(DestacadosColecctionMotos) { item ->
+                Destacados(item.imagenResId, item.nombreResId, item.precioResId)
+            }
         }
-    }
 
+    }
 }
 @Composable
 private fun BarraDeNavegacion(modifier: Modifier = Modifier) {
---
+++ app/src/main/java/com/pmydm/practica04/Prueba.kt
@@ -0,0 +1,17 @@
+package com.pmydm.practica04
+
+import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+
+
+fun modificar(
+    modifier: Modifier = Modifier,
+
+
+) {
+
+categoria++
+
+}
